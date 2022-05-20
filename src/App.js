dadcd9a76b2d1d06807cd44ec9237dd837f32198
rafignwn <figun746@gmail.com>
2022-05-20 22:41:37 +0700

    menambahkan footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import TitleProject from "./component/Header/TitleProject";
 import { Routes, Route } from "react-router-dom";
 import MonitoringPh from "./component/MonitoringPh/MonitoringPh";
 import MonitroingPakan from "./component/MonitoringPakan/MonitoringPakan";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
-    <div className="container">
-      <TitleProject />
-      <Routes>
-        <Route path="/" element={<InfoBoard />} />
-        <Route path="/monitoring-ph" element={<MonitoringPh />} />
-        <Route path="/monitoring-pakan" element={<MonitroingPakan />} />
-      </Routes>
-    </div>
+    <>
+      <div className="container">
+        <TitleProject />
+        <Routes>
+          <Route path="/" element={<InfoBoard />} />
+          <Route path="/monitoring-ph" element={<MonitoringPh />} />
+          <Route path="/monitoring-pakan" element={<MonitroingPakan />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
