6a9ff0966596a32a9e45bfa2e5dfda28594f7656
rafignwn <figun746@gmail.com>
2022-06-07 09:31:21 +0700

    update halaman monitoring pakan jadi realtime

--- src/component/MonitoringPakan/MonitoringPakan.jsx
+++ src/component/MonitoringPakan/MonitoringPakan.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import ItemInfo from "../ItemInfo/ItemInfo";
 import { format, parseISO } from "date-fns";
 import { id } from "date-fns/locale";
@@ -9,69 +9,89 @@ import axios from "axios";
 
 export default function MonitroingPakan() {
   const [sisaPakan, setSisaPakan] = useState(0);
+  const [fixDataPakan, setFixDataPakan] = useState(null);
 
-  const dataPakanDefault = {
-    items: ["Jam Pemberian Pakan Ikan", "Penggunaan Terakhir Pakan Ikan"],
-    values: [["07:00", "13:00", "17:00"]],
-    link: {
-      title: "Beri Pakan Ikan",
-      type: "button",
-      action: async function (e) {
-        console.log("Sedang memberi pakan ikan");
-        e.currentTarget.classList.add("btn-wait");
-        e.currentTarget.textContent = "Sedang Memberi Pakan...";
-        let intervalPakan = setInterval(async () => {
-          const res = await axios.get(
-            "https://primus.somee.com/checkTimeToFeed"
+  function parseTime(waktu) {
+    return format(parseISO(waktu), "eeeeeeee, dd MMM yyyy HH:mm:ss", {
+      locale: id,
+    });
+  }
+
+  async function getDataPakan() {
+    const res = await axios.get("https://primus.somee.com/feedShortRecords");
+    return res.data;
+  }
+
+  const dataPakanDefault = useMemo(() => {
+    return {
+      items: ["Jam Pemberian Pakan Ikan", "Penggunaan Terakhir Pakan Ikan"],
+      values: [["07:00", "13:00", "17:00"]],
+      link: {
+        title: "Beri Pakan Ikan",
+        type: "button",
+        action: async function (e) {
+          console.log("Sedang memberi pakan ikan");
+          e.currentTarget.classList.add("btn-wait");
+          e.currentTarget.textContent = "Sedang Memberi Pakan...";
+          let intervalPakan = setInterval(async () => {
+            const res = await axios.get(
+              "https://primus.somee.com/checkTimeToFeed"
+            );
+            console.log(res.data);
+            if (res.data === 0) {
+              const btnPakan =
+                document.querySelector(".btn-wait") ||
+                document.querySelector(".btn-goal");
+              // update data pakan
+              const newData = await getDataPakan();
+              dataPakanDefault.values = [
+                ["07:00", "13:00", "17:00"],
+                [`${newData.beratPakan} kg`, parseTime(newData.waktuPakan)],
+              ];
+              setFixDataPakan(dataPakanDefault);
+              setSisaPakan(newData.sisaPakan);
+              // pesan berhasil
+              btnPakan.classList.remove("btn-wait");
+              btnPakan.classList.add("btn-goal");
+              btnPakan.textContent = "Berhasil Memberi Pakan";
+              // delay 3 detik
+              await new Promise((resolve) => setTimeout(resolve, 3000));
+              // set button ke defaul
+              btnPakan.classList.remove("btn-goal");
+              btnPakan.textContent = "Beri Pakan Ikan";
+              // clear interval cek kondisi makan
+              clearInterval(intervalPakan);
+            }
+          }, 3000);
+          const response = await axios.get(
+            "https://primus.somee.com/feedTheFish"
           );
-          console.log(res.data);
-          if (res.data === 0) {
-            const btnPakan =
-              document.querySelector(".btn-wait") ||
-              document.querySelector(".btn-goal");
-            // pesan berhasil
-            btnPakan.classList.remove("btn-wait");
-            btnPakan.classList.add("btn-goal");
-            btnPakan.textContent = "Berhasil Memberi Pakan";
-            // delay 3 detik
-            await new Promise((resolve) => setTimeout(resolve, 3000));
-            // set button ke defaul
-            btnPakan.classList.remove("btn-goal");
-            btnPakan.textContent = "Beri Pakan Ikan";
-            clearInterval(intervalPakan);
-          }
-        }, 3000);
-        const response = await axios.get(
-          "https://primus.somee.com/feedTheFish"
-        );
-        console.log(response.data);
+          console.log(response.data);
+        },
       },
-    },
-  };
+    };
+  }, []);
 
   const [dataPakan, setDataPakan] = useState(dataPakanDefault);
 
   useEffect(() => {
-    fetch("https://primus.somee.com/feedShortRecords")
-      .then((res) => res.json())
-      .then((data) => {
+    console.log("render mon pakan");
+    if (fixDataPakan) {
+      setDataPakan(fixDataPakan);
+    } else {
+      (async function () {
+        const data = await getDataPakan();
         setSisaPakan(data.sisaPakan);
-        const waktu_pakan = format(
-          parseISO(data.waktuPakan),
-          "eeeeeeee, dd MMM yyyy HH:mm:ss",
-          {
-            locale: id,
-          }
-        );
+
         dataPakanDefault.values = [
           ...dataPakanDefault.values,
-          [`${data.beratPakan} kg`, waktu_pakan],
+          [`${data.beratPakan} kg`, parseTime(data.waktuPakan)],
         ];
         setDataPakan(dataPakanDefault);
-      })
-      .catch((err) => console.log(err));
+      })();
+    }
     // eslint-disable-next-line
-  }, []);
+  }, [fixDataPakan, dataPakan]);
 
   return (
     <div className="mt-30">
